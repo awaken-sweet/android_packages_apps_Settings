c5a4a1f5b98ef1449d59c36675d2b029de72410b
Cyber Knight <cyberknight755@gmail.com>
2024-07-31 23:09:25 +0800

    Settings: Make force LTE_CA toggle opt-in
    - This is due to the fact that most devices do not need this hack;
    - However there are certain devices that do so we shall make it opt-in.
    
    Change-Id: I323376af436f39b861eda32ddb37a4e4b7cb2fdd
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/com/android/settings/network/telephony/ForceLteCaPreferenceController.java
+++ src/com/android/settings/network/telephony/ForceLteCaPreferenceController.java
@@ -18,6 +18,7 @@ import com.android.internal.telephony.util.ArrayUtils;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnStart;
 import com.android.settingslib.core.lifecycle.events.OnStop;
+import com.android.settings.R;
 
 /**
  * Preference controller for "Force LTE_CA" hack
@@ -70,10 +71,15 @@ public class ForceLteCaPreferenceController extends TelephonyTogglePreferenceCon
 
     @Override
     public int getAvailabilityStatus(int subId) {
+
+        if (!mContext.getResources().getBoolean(R.bool.config_showForceLteCaToggle))
+            return UNSUPPORTED_ON_DEVICE;
+
         init(subId);
         if (SubscriptionManager.isValidSubscriptionId(mSubId)) {
             return AVAILABLE;
         }
+
         return CONDITIONALLY_UNAVAILABLE;
     }
 
